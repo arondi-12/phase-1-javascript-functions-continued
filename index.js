3a3d3dc02a9a1bfa0aa60c5f76497dfe48c5061a
arondi-12 <davieariwi@gmail.com>
2024-10-10 22:52:31 +0300

    Functions lab complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+function saturdayFun(acivity = 'roller-skate'){
+    return `This Saturday, I want to ${acivity}!`;
+}
+saturdayFun();
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(){
+    const innerFunction = function (input = 'special'){
+        return `You are ${input}!`;
+    }
+}
+
+function wrapAdjective(wrapper = '*') {
+    // Return a new function that takes an adjective and returns the complete phrase
+    return function(adjective) {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
+  
+  
